ab15889a7702a1052b927a3375869e24c63f742c
aesaandres <aesa.andres.1997@gmail.com>
2025-11-27 09:21:22 -0400

    cambio de contacto

--- app/components/contact.tsx
+++ app/components/contact.tsx
@@ -10,7 +10,7 @@ export default function Contact() {
        <section id="contact" className="py-20 bg-primary from-primary/20 to-brand-dark">
       <div className="container mx-auto px-4 md:px-6">
         <div className="max-w-4xl mx-auto bg-terciary border border-cuaternary/5 rounded-2xl p-8 md:p-12 shadow-2xl">
-          <div className="grid md:grid-cols-2 gap-12">
+          <div className="grid md:grid-cols-2  grid-cols-1 gap-12">
             
             <div className="space-y-8">
               <div>
@@ -32,28 +32,28 @@ export default function Contact() {
                   <div className="p-3 bg-primary rounded-full text-segundary group-hover:scale-110 transition-transform">
                     <Mail size={20} />
                   </div>
-                  <span className="font-medium">aesa.andres.1997@gmail.com</span>
+                  <span className="font-medium text-xs  md:text-base">aesa.andres.1997@gmail.com</span>
                 </a>
                 
                 <a href="https://github.com/Aesaandres" target="_blank" rel="noreferrer" className="flex items-center space-x-4 text-brand-light hover:text-brand-accent transition-colors p-4 bg-primary/50 rounded-lg border border-cuaternary/5 hover:border-segundary/30 group">
                   <div className="p-3 bg-primary rounded-full text-segundary group-hover:scale-110 transition-transform">
                     <Github size={20} />
                   </div>
-                  <span className="font-medium">github.com/aesaandres</span>
+                  <span className="font-medium text-xs  md:text-base">github.com/aesaandres</span>
                 </a>
 
                 <a href="www.linkedin.com/in/andres-sanchez-6266701a0" className="flex items-center space-x-4 text-cuaternary hover:text-brand-accent transition-colors p-4 bg-primary/50 rounded-lg border border-cuaternary/5 hover:border-segundary/30 group">
                   <div className="p-3 bg-primary rounded-full text-segundary group-hover:scale-110 transition-transform">
                     <Linkedin size={20} />
                   </div>
-                  <span className="font-medium">linkedin.com/in/andres-sanchez</span>
+                  <span className="font-medium text-xs  md:text-base">linkedin.com/in/andres-sanchez</span>
                 </a>
 
                 <a href="https://www.instagram.com/faludinn/" className="flex items-center space-x-4 text-brand-light hover:text-brand-accent transition-colors p-4 bg-primary/50 rounded-lg border border-cuaternary/5 hover:border-segundary/30 group">
                   <div className="p-3 bg-primary rounded-full text-segundary group-hover:scale-110 transition-transform">
                     <Instagram size={20} />
                   </div>
-                  <span className="font-medium">FaludinDev</span>
+                  <span className="font-medium text-xs  md:text-base">FaludinDev</span>
                 </a>
               </div>
               
